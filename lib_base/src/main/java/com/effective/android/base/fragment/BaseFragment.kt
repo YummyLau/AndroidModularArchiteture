f094f77976e6a15868ddb07a312a1bdc09ec22af
yummylau <yummyl.lau@gmail.com>
2019-06-27 15:57:13 +0800

    自定义框架切换为jimu支持，后续增强功能独立测试再上线

--- lib_base/src/main/java/com/effective/android/base/fragment/BaseFragment.kt
+++ lib_base/src/main/java/com/effective/android/base/fragment/BaseFragment.kt
@@ -1,11 +1,11 @@
 package com.effective.android.base.fragment
 
 import android.os.Bundle
-import android.support.annotation.LayoutRes
-import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.fragment.app.Fragment
 
 /**
  * Created by yummyLau on 2018/4/26.
