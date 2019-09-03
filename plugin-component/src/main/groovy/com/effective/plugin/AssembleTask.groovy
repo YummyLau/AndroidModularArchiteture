4edd3343211958022ac9263a8b3fd5e4867d0cc3
yummylau <yummyl.lau@gmail.com>
2019-09-03 21:10:22 +0800

    删除dd插件，升级插件支持代码隔离，暂时配置libBase，持续更新

--- plugin-component/src/main/groovy/com/effective/plugin/AssembleTask.groovy
+++
@@ -1,7 +0,0 @@
-package com.effective.plugin;
-
-class AssembleTask {
-    public boolean isAssemble = false
-    public boolean isDebug = false
-    public List<String> modules = new ArrayList<>()
-}
