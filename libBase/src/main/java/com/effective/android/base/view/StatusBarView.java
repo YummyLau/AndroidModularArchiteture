b9d9e031ea31129bd7fe09ae6cd7f205ac2a6c42
yummylau <yummyl.lau@gmail.com>
2019-09-19 19:56:12 +0800

    新增状态栏view

---
+++ libBase/src/main/java/com/effective/android/base/view/StatusBarView.java
@@ -0,0 +1,83 @@
+package com.effective.android.base.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Build;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.annotation.ColorRes;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+
+import com.effective.android.base.R;
+
+import java.lang.reflect.Field;
+
+
+;
+
+/**
+ * 状态栏view
+ * Created by yummylau on 2017/9/09.
+ */
+
+public class StatusBarView extends LinearLayout {
+
+    @ColorRes
+    private int mStatusBarColor;
+
+    public StatusBarView(Context context) {
+        this(context, null);
+    }
+
+    public StatusBarView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public StatusBarView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView(attrs, defStyleAttr);
+    }
+
+    private void initView(AttributeSet attrs, int defStyle) {
+        View root = LayoutInflater.from(getContext()).inflate(R.layout.base_statusbar_layout, this, true);
+        final TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.StatusBarView, defStyle, 0);
+        if (typedArray != null) {
+            mStatusBarColor = typedArray.getResourceId(R.styleable.StatusBarView_color, R.color.colorPrimary);
+        } else {
+            mStatusBarColor = R.color.colorPrimary;
+        }
+        fitSpecialModelStatusBar();
+        setBackgroundColor(ContextCompat.getColor(getContext(), mStatusBarColor));
+        int statusbarHeight = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT ? getStatusBarHeight(getContext()) : 0;
+        root.getLayoutParams().height = statusbarHeight;
+    }
+
+
+    public static int getStatusBarHeight(Context context) {
+        int sbar = 0;
+        try {
+            Class c = Class.forName("com.android.internal.R$dimen");
+            Object obj = c.newInstance();
+            Field field = c.getField("status_bar_height");
+            int x = Integer.parseInt(field.get(obj).toString());
+            sbar = context.getResources().getDimensionPixelSize(x);
+        } catch (Exception var7) {
+            var7.printStackTrace();
+        }
+        return sbar;
+    }
+
+    /**
+     * 特殊机型的状态栏处理.
+     */
+    private void fitSpecialModelStatusBar() {
+        if (TextUtils.equals(Build.MODEL, "vivo X6S A")) {
+            mStatusBarColor = R.color.transparent;
+        }
+    }
+}
--- libBase/src/main/java/com/effective/android/base/view/TitleView.java
+++ libBase/src/main/java/com/effective/android/base/view/TitleView.java
@@ -1,21 +1,42 @@
 package com.effective.android.base.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.effective.android.base.R;
 
 public class TitleView extends RelativeLayout {
 
+    private TextView title;
+
     public TitleView(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public TitleView(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public TitleView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initView(context, attrs, defStyleAttr);
     }
 
+    private void initView(Context context, AttributeSet attrs, int defStyleAttr) {
+        View root = LayoutInflater.from(getContext()).inflate(R.layout.base_title_layout, this, true);
+        title = root.findViewById(R.id.title);
+        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.TitleView, defStyleAttr, 0);
+        title.setText(typedArray.getString(R.styleable.TitleView_title));
+        setBackgroundColor(typedArray.getColor(R.styleable.TitleView_background, Color.TRANSPARENT));
+    }
+
+    public TextView getTitle() {
+        return title;
+    }
 }
