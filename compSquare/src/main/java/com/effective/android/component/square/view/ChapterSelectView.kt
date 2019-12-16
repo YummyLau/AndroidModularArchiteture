a06bae3f0a8f9d2b02ea41809b7b977faeb1540d
yummylau <yummyl.lau@gmail.com>
2019-12-16 16:53:26 +0800

    自定义编辑器

--- compSquare/src/main/java/com/effective/android/component/square/view/ChapterSelectView.kt
+++ compSquare/src/main/java/com/effective/android/component/square/view/ChapterSelectView.kt
@@ -7,6 +7,8 @@ import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 import android.widget.RelativeLayout
+import android.widget.RelativeLayout.ALIGN_PARENT_RIGHT
+import android.widget.ScrollView
 import android.widget.TextView
 import com.effective.android.base.util.DisplayUtils
 import com.effective.android.base.util.ResourceUtils
@@ -23,8 +25,9 @@ import com.effective.android.component.square.listener.OnEditListener
  * 章节选择器，用于用户自主选择章节，可复用于任何文章列表
  * created by yummylau on 2019/12/16
  */
-class ChapterSelectView : RelativeLayout {
+class ChapterSelectView : ScrollView {
 
+    private lateinit var container: RelativeLayout;
     private lateinit var dontTitleView: TextView
     private lateinit var dontDragFlowLayout: DragFlowLayout
     private lateinit var todoTitleView: TextView
@@ -50,6 +53,11 @@ class ChapterSelectView : RelativeLayout {
     }
 
     private fun initView(context: Context) {
+
+        container = RelativeLayout(context)
+        val containerLp = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
+        addView(container, containerLp)
+
         dontTitleView = TextView(context)
         dontTitleView.id = View.generateViewId()
         dontTitleView.textSize = 15f
@@ -57,17 +65,18 @@ class ChapterSelectView : RelativeLayout {
         dontTitleView.typeface = defaultFromStyle(BOLD)
         dontTitleView.text = ResourceUtils.getString(context, R.string.square_list_edit_done)
         val dontTitleViewLp = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
-        addView(dontTitleView, dontTitleViewLp)
+        dontTitleViewLp.topMargin = DisplayUtils.dip2px(context,3f)
+        container.addView(dontTitleView, dontTitleViewLp)
 
         action = TextView(context)
         action.id = View.generateViewId()
-        action.textSize = 15f
+        action.textSize = 13f
         action.setTextColor(ResourceUtils.getColor(context, R.color.colorThemeText))
         action.text = ResourceUtils.getString(context, R.string.square_edit)
-        action.background = ResourceUtils.getDrawable(context, R.drawable.base_se_btn)
+        action.background = ResourceUtils.getDrawable(context, R.drawable.square_se_chatper_selector_action)
         action.setPadding(DisplayUtils.dip2px(context, 8f), DisplayUtils.dip2px(context, 5f), DisplayUtils.dip2px(context, 8f), DisplayUtils.dip2px(context, 5f))
-        val actionLp = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
-        actionLp.addRule(ALIGN_PARENT_RIGHT)
+        val actionLp = RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
+        actionLp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT)
         action.setOnClickListener {
             if (action.text == context.getString(R.string.square_edit)) {
                 dontDragFlowLayout.beginDrag()
@@ -80,13 +89,13 @@ class ChapterSelectView : RelativeLayout {
                 listenter?.onFinish(dontDragFlowLayout.dragItemManager.getItems(), todoDragFlowLayout.dragItemManager.getItems())
             }
         }
-        addView(action, actionLp)
+        container.addView(action, actionLp)
 
         dontDragFlowLayout = DragFlowLayout(context)
         dontDragFlowLayout.id = View.generateViewId()
-        val dontDragFlowLayoutLp = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
+        val dontDragFlowLayoutLp = RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
         dontDragFlowLayoutLp.topMargin = DisplayUtils.dip2px(context, 10f)
-        dontDragFlowLayoutLp.addRule(BELOW, dontTitleView.id)
+        dontDragFlowLayoutLp.addRule(RelativeLayout.BELOW, dontTitleView.id)
         dontDragFlowLayout.lineSpacing = DisplayUtils.dip2px(context, 5f)
         dontDragFlowLayout.itemSpacing = DisplayUtils.dip2px(context, 5f)
         dontDragFlowLayout.setOnDragStateChangeListener { dfl, dragState ->
@@ -131,7 +140,7 @@ class ChapterSelectView : RelativeLayout {
 
             override fun getData(itemView: View?): DraggableChapter = itemView?.tag as DraggableChapter
         })
-        addView(dontDragFlowLayout, dontDragFlowLayoutLp)
+        container.addView(dontDragFlowLayout, dontDragFlowLayoutLp)
 
         todoTitleView = TextView(context)
         todoTitleView.id = View.generateViewId()
@@ -139,17 +148,17 @@ class ChapterSelectView : RelativeLayout {
         todoTitleView.setTextColor(ResourceUtils.getColor(context, R.color.colorTextPrimary))
         todoTitleView.typeface = defaultFromStyle(BOLD)
         todoTitleView.text = ResourceUtils.getString(context, R.string.square_list_to_edit)
-        val todoTitleViewLp = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
+        val todoTitleViewLp = RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
         todoTitleViewLp.topMargin = DisplayUtils.dip2px(context, 10f)
-        todoTitleViewLp.addRule(BELOW, dontDragFlowLayout.id)
-        addView(todoTitleView, todoTitleViewLp)
+        todoTitleViewLp.addRule(RelativeLayout.BELOW, dontDragFlowLayout.id)
+        container.addView(todoTitleView, todoTitleViewLp)
 
 
         todoDragFlowLayout = DragFlowLayout(context)
         todoDragFlowLayout.id = View.generateViewId()
-        val todoDragFlowLayoutLp = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
+        val todoDragFlowLayoutLp = RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
         todoDragFlowLayoutLp.topMargin = DisplayUtils.dip2px(context, 10f)
-        todoDragFlowLayoutLp.addRule(BELOW, todoTitleView.id)
+        todoDragFlowLayoutLp.addRule(RelativeLayout.BELOW, todoTitleView.id)
         todoDragFlowLayout.lineSpacing = DisplayUtils.dip2px(context, 5f)
         todoDragFlowLayout.itemSpacing = DisplayUtils.dip2px(context, 5f)
         todoDragFlowLayout.setDragAdapter(object : DragAdapter<DraggableChapter>() {
@@ -180,13 +189,14 @@ class ChapterSelectView : RelativeLayout {
             override fun getData(itemView: View?): DraggableChapter = itemView?.tag as DraggableChapter
         })
         todoDragFlowLayout.setDraggable(false)
-        addView(todoDragFlowLayout, todoDragFlowLayoutLp)
+        container.addView(todoDragFlowLayout, todoDragFlowLayoutLp)
 
         setOnClickListener {
             //默认拦截
         }
 
         val padding = DisplayUtils.dip2px(context, 10f)
+        isFillViewport = true
         setPadding(padding, padding, padding, padding)
     }
 
