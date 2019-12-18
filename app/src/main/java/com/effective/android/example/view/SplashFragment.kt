32514b8859e514d39c9d0ddafbce550c60938232
yummylau <yummyl.lau@gmail.com>
2019-12-18 14:21:57 +0800

    完整的闪屏模块，3秒延迟进入主页

--- app/src/main/java/com/effective/android/example/view/SplashFragment.kt
+++ app/src/main/java/com/effective/android/example/view/SplashFragment.kt
@@ -1,10 +1,17 @@
 package com.effective.android.example.view
 
+import android.graphics.Typeface
 import android.os.Bundle
 import com.effective.android.base.fragment.BaseVmFragment
+import com.effective.android.base.util.ResourceUtils
 import com.effective.android.example.R
 import com.effective.android.example.vm.SplashVm
+import io.reactivex.Flowable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.app_fragment_splash.*
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 /**
  * 闪屏模块
@@ -12,19 +19,39 @@ import kotlinx.android.synthetic.main.app_fragment_splash.*
  */
 class SplashFragment : BaseVmFragment<SplashVm>() {
 
+    private var disposable: Disposable? = null
+
     override fun getViewModel(): Class<SplashVm> = SplashVm::class.java
 
     override fun getLayoutRes(): Int = R.layout.app_fragment_splash
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        loadAd()
+        initView()
     }
 
-    private fun loadAd(){
+    private fun initView() {
+        appTip.typeface = Typeface.createFromAsset(context?.assets, ResourceUtils.getString(activity!!, R.string.app_splash_text_font))
         splashSkip.setOnClickListener {
-            (activity as MainActivity).toHome()
+            toHome()
         }
-//        splashImage.setImageDrawable(R.drawable.app_ic_blog)
+        splashImage.setImageDrawable(ResourceUtils.getDrawable(activity!!, String.format(activity!!.getString(R.string.app_splash_image_prefix), (Random().nextInt(7) + 1).toString())))
+        disposable = Flowable.timer(3000, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    toHome()
+                }, {
+                    toHome()
+                })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        disposable?.dispose()
+    }
+
+    private fun toHome() {
+        disposable?.dispose()
+        (activity as MainActivity).toHome()
     }
 }
