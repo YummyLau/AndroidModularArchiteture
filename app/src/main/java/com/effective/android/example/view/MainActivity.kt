75cf41f5c152f7ce13fb8461452b0e0761ca06e7
yummylau <yummyl.lau@gmail.com>
2019-12-19 16:49:12 +0800

    闪屏预留主页面loading空间

--- app/src/main/java/com/effective/android/example/view/MainActivity.kt
+++ app/src/main/java/com/effective/android/example/view/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.effective.android.example.view
 
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
 import com.effective.android.base.activity.BaseActivity
 import com.effective.android.base.systemui.StatusbarHelper
 import com.effective.android.base.util.FragmentUtils
 import com.effective.android.base.util.ResourceUtils
 import com.effective.android.example.R
+import kotlinx.android.synthetic.main.app_activity_main_layout.*
 
 
-class MainActivity : BaseActivity(){
+class MainActivity : BaseActivity() {
 
     override fun getLayoutRes(): Int {
         return R.layout.app_activity_main_layout
@@ -16,16 +19,18 @@ class MainActivity : BaseActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        StatusbarHelper.setStatusBarColor(this,ResourceUtils.getColor(this,R.color.transparent))
+        StatusbarHelper.setStatusBarColor(this, ResourceUtils.getColor(this, R.color.transparent))
         toSplash()
     }
 
-    private fun toSplash(){
-        FragmentUtils.replace(supportFragmentManager,R.id.fragment_container,SplashFragment())
+    private fun toSplash() {
+        FragmentUtils.replace(supportFragmentManager, R.id.splashContainer, SplashFragment())
+        FragmentUtils.replace(supportFragmentManager, R.id.mainContainer, MainFragment())
     }
 
-    fun toHome(){
-        FragmentUtils.replace(supportFragmentManager,R.id.fragment_container,MainFragment())
+    fun toHome() {
+        splashContainer.visibility = View.GONE
+        mainContainer.visibility = View.VISIBLE
     }
 }
 
